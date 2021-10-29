b8be9617ab3ac5ce3b1e0a1e0593fa9b04e26723
ilker <ilker.uyanik11@gmail.com>
2021-10-29 21:01:19 +0300

    Review: release date format changed

--- src/components/movieComponent/movie.jsx
+++ src/components/movieComponent/movie.jsx
@@ -16,6 +16,9 @@ export default function Movie(props){
     if(release_date===undefined){
         release_date = movie_data.first_air_date;
     }
+    
+    const release_date_arr = release_date.split('-');
+    release_date = release_date_arr[2]+'-'+release_date_arr[1]+'-'+release_date_arr[0];
 
     return (
         <div className='movie'>
